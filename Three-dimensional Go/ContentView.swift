a60e0b48f42e16d2334607e5767963865f214616
笑尘 <xiaochenl894@gmail.com>
2026-09-02 14:56:34 +0800

    Initial Commit

---
+++ Three-dimensional Go/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Three-dimensional Go
+//
+//  Created by 红尘一笑 on 2026/9/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Three-dimensional Go/Three_dimensional_GoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_dimensional_GoApp.swift
+//  Three-dimensional Go
+//
+//  Created by 红尘一笑 on 2026/9/2.
+//
+
+import SwiftUI
+
+@main
+struct Three_dimensional_GoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
